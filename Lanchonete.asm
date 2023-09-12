99ada7be6371bf8e5c388e24ca0bfccf1fe113a0
muzanpvp <mkibutsuji4@gmail.com>
2023-09-12 17:48:33 -0300

    Implementação do projeto Lanchonete McMissil.

--- Lanchonete.asm
+++ Lanchonete.asm
@@ -1,7 +1,57 @@
-#text:
+#Este projeto foi desenvolvido por : Marcos Venício da Palma Dias, Icaro Gabriel Almeida Reis, André Luiz Almeida Lima
+#Para o projeto em Assembly da disciplina de Organização e arquitetura de computadores
+
+.data
+
+	
+	#menu view
+	mensagemBemVindo: .asciiz "Seja bem vindo a Lanchonete McMissil"
+	pedido: .asciiz "1.Realizar pedido"
+	sair: .asciiz "2.Sair"	
+	
+	#cardapio view
+	mensagemCardapio: .asciiz "Escolha seus produtos!"
+	hamburguer: .asciiz "1- Hamburguer R$5,00"
+	misto: .asciiz "2- Misto R$3,50"
+	xbacon: .asciiz "3- X-Bacon R$8,00"
+	xcalabresa: .asciiz "4- X-Calabresa R$9,00"
+	xchurrasco: .asciiz "5- X-Churrasco R$12,00"
+	xtudo: .asciiz "6- X-Tudo R$14,00"
+	cachorroQuente: .asciiz "7- Cachorro Quente R$4,00"
+	batataFrita: .asciiz "8- Batata Frita Tamanho M- R$5,00 "
+	refrigerante: .asciiz "9-Refrigerante 2L R$6,00"
+	suco: .asciiz "Suco de Maracujá R$2,00"
+	
+	
+	#pagamento view
+	valor: 
+	valorTotal: 
+	notaFiscal:
+	endereco: 	
+	mensagemAgradecimento: .asciiz "Muito obrigado pela preferência! A Lanchonete McMissil agradece! "
+			
+			
+.text
 
 
 
+#login
 
 
-#data: 
+
+#cadastro
+
+
+
+#menu
+
+
+
+#cardapio
+
+
+
+#pagamento
+
+
+
